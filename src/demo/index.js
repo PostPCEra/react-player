7edd2e254ade65fce6f0f774cb03f26c4d321574
PostPcEra <vision21c@gmail.com>
2020-06-28 10:28:44 -0700

    React.fragment added

--- src/demo/index.js
+++ src/demo/index.js
@@ -5,3 +5,6 @@ import App from './App'
 const container = document.getElementById('app')
 
 render(<App />, container)
+// SR : code added in App.js file
+// Multiple compoents in react (with React.fragments)
+// https://stackoverflow.com/questions/32577886/is-there-a-way-to-render-multiple-react-components-in-the-react-render-functio
