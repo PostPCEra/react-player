22f0e996f2d0de3bc0e11232fe060df3e3508429
PostPcEra <vision21c@gmail.com>
2020-06-29 01:37:22 -0700

    React-table code added

---
+++ src/demo/App2.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class App2 extends Component {
+    render() {
+      return <h1>Hello, 333 22222222222222222222222222 {this.props.name}</h1>;
+    }
+  }
+
+  export default App2
